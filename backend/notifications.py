3db9f53765c27a85d5f3949e02bedcbe4955bddb
Harsh Modi <harshmodi@google.com>
2017-05-10 12:38:52 -0700

    notifications: Fix the query to actually work correctly.

--- backend/notifications.py
+++ backend/notifications.py
@@ -11,6 +11,8 @@ def HandleNotification(firebase, notification_id, notification):
   elif 'groupId' in notification:
     players = firebase.get('/groups/%s' % notification['groupId'],
                            'players')
+    if players is None:
+      players = []
   else:
     logging.error('Notification %s does not have a playerId or a groupId!' % (
         notification_id))
@@ -26,12 +28,19 @@ def ExecuteNotifications(request, firebase):
 
   This method will execute queued notifications and send them to apps and add
   them to the player specific notification database URLs.
+
+  This method works by fetching the last 20 notifications that are less than
+  the current time. Once the notifications are handled, their time is set to 0
+  indicating that they have been handled. Since now that they are zero, all
+  future notifications must have a newer timestamp, and thus will show up in
+  the query first.
   """
   # Handle notifications, 20 at a time so we don't need to get the entire
   # notification database.
   get_params = {
-      'orderBy': '"sent"',
-      'limitToLast': 20
+      'orderBy': '"sendTime"',
+      'limitToLast': 20,
+      'endAt': int(time.time())
   }
   current_time = int(time.time())
   while True:
@@ -40,10 +49,10 @@ def ExecuteNotifications(request, firebase):
     if notifications is None:
       return
     for notification_id, notification in notifications.iteritems():
-      if 'sent' not in notification and current_time > int(notification['sendTime']):
+      if notification['sendTime'] > 0 and current_time > int(notification['sendTime']):
         updates = True
         HandleNotification(firebase, notification_id, notification)
-        notifications[notification_id]['sent'] = True
+        notifications[notification_id]['sendTime'] = 0
     if updates:
       result = firebase.patch('/notifications', notifications)
       if result is None:
